0b7e19628b3f405a9bef4a09147bb56d820b52ff
Helen Langridge <langridge.helen@outlook.com>
2022-10-16 19:43:01 +0100

    getters and setters complete

--- src/main/java/flight/Flight.java
+++ src/main/java/flight/Flight.java
@@ -28,4 +28,68 @@ public class Flight {
         this.passengers = new ArrayList<Passenger>();
     }
 
+    public String getFlightNumber(){
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber){
+        this.flightNumber = flightNumber;
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
+
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+
+    public String getDepartureAirport(){
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport){
+        this.departureAirport = departureAirport;
+    }
+
+    public String getDestinationAirport(){
+        return destinationAirport;
+    }
+
+    public void setDestinationAirport(String destinationAirport){
+        this.destinationAirport = destinationAirport;
+    }
+
+    public String getDepartureTime(){
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime){
+        this.departureTime = departureTime;
+    }
+
+    public void addPilot(Pilot pilot){
+        this.pilots.add(pilot);
+    }
+
+    public void addCrewMember(CabinCrewMember cabinCrewMember){
+        this.cabinCrewMembers.add(cabinCrewMember);
+    }
+
+    public void addPassenger(Passenger passenger){
+        this.passengers.add(passenger);
+    }
+
+    public int getPilotListLength(){
+        return pilots.size();
+    }
+
+    public int getCabinCrewListLength(){
+        return cabinCrewMembers.size();
+    }
+
+    public int getPassengerListLength(){
+        return passengers.size();
+    }
+
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,9 +1,12 @@
 import flight.Flight;
 import org.junit.Before;
+import org.junit.Test;
 import people.CabinCrewMember;
 import people.Passenger;
 import people.Pilot;
 import people.Rank;
+
+import static org.junit.Assert.assertEquals;
 
 public class FlightTest {
 
@@ -24,6 +27,29 @@ public class FlightTest {
         passenger1 = new Passenger("Mike", 1);
         passenger2 = new Passenger("Helen", 2);
         passenger3 = new Passenger("Jasmin", 3);
+    }
+
+    @Test
+    public void canAddPilot(){
+        flight.addPilot(pilot1);
+        assertEquals(1, flight.getPilotListLength());
+    }
+
+    @Test
+    public void canAddCrewMembers(){
+        flight.addCrewMember(cabinCrewMember);
+        assertEquals(1, flight.getCabinCrewListLength());
+    }
+
+    @Test
+    public void canAddPassengers(){
+        flight.addPassenger(passenger1);
+        assertEquals(1, flight.getPassengerListLength());
+    }
+
+    @Test
+    public void flightHasFlightNumber(){
+        assertEquals("KEH3895", flight.getFlightNumber());
     }
 
 }
