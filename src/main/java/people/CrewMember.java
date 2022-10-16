2d2d42c001ee1d5841c2b107297135f6758ed3be
Helen Langridge <langridge.helen@outlook.com>
2022-10-16 18:41:42 +0100

    setup crewmember

---
+++ src/main/java/people/CrewMember.java
@@ -0,0 +1,13 @@
+package people;
+
+public class CrewMember extends Person {
+
+    private Rank rank;
+
+    public CrewMember(String name, Rank rank){
+        super(name);
+        this.rank = rank;
+    }
+    
+
+}
