5fb5a0eed705b49277afc94c41b3a5235e4df17c
Helen Langridge <langridge.helen@outlook.com>
2022-10-16 18:58:05 +0100

    pilot tests complete:

--- src/main/java/people/Pilot.java
+++ src/main/java/people/Pilot.java
@@ -1,4 +1,16 @@
 package people;
 
-public class Pilot {
+public class Pilot extends CrewMember{
+
+    private String pilotLicenseNumber;
+
+    public Pilot(String name, Rank rank, String pilotLicenseNumber){
+        super( name, rank);
+        this.pilotLicenseNumber = pilotLicenseNumber;
+    }
+
+    public String getPilotLicenseNumber(){
+        return pilotLicenseNumber;
+    }
+
 }
--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,2 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+import people.Pilot;
+import people.Rank;
+
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
+
+    Pilot pilot;
+
+    @Before
+    public void before() {
+        pilot = new Pilot("Gilbert", Rank.CAPTAIN, "324t324");
+    }
+
+    @Test
+    public void pilotHasName(){
+        assertEquals("Gilbert", pilot.getName());
+    }
+
+    @Test
+    public void pilotHasRank(){
+        assertEquals(Rank.CAPTAIN, pilot.getRank());
+    }
+
+    @Test
+    public void pilotHasLicenseNumber(){
+        assertEquals("324t324", pilot.getPilotLicenseNumber());
+    }
+
 }
