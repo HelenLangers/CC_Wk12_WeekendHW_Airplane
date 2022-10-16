667dcf34868e597c39470dfc3ede7385f385ea64
Helen Langridge <langridge.helen@outlook.com>
2022-10-16 18:34:55 +0100

    create person abstract class

---
+++ src/main/java/people/Passenger.java
@@ -0,0 +1,11 @@
+package people;
+
+public class Passenger extends Person {
+
+    private int numberOfBags;
+
+    public Passenger(int numberOfBags){
+        super(name);
+        this.numberOfBags = numberOfBags;
+    }
+}
